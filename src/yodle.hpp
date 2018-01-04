bc1a954debb394b0a9f1c7311bdf81f1f87b63ea
Jan Olszak <jan.olszak@vestiacom.com>
2018-01-04 10:12:42 +0100

    Helper header file
    Change-Id: I2be13d0e1e6ba6eae637d30ac8468deab623c81f

---
+++ src/yodle.hpp
@@ -0,0 +1,10 @@
+#ifndef YODLE_YODLE_HPP_
+#define YODLE_YODLE_HPP_
+
+// Helper header with all other headers needed to setup libyodle
+
+#include "types.hpp"
+#include "channel.hpp"
+#include "message.hpp"
+
+#endif // YODLE_YODLE_HPP_
