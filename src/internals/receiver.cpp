bbdaa2bb37464eca0a3d9ee25e7f6c2c145026ec
Jan Olszak <jan.olszak@vestiacom.com>
2018-04-26 15:16:18 +0200

    Catch unexpected exceptions in libev callbacks
    Change-Id: I3a59e25f6eb0e3a401f11da9ba8b2c9991fe5df9

--- src/internals/receiver.cpp
+++ src/internals/receiver.cpp
@@ -18,192 +18,201 @@ namespace internals {
 Receiver::Receiver(int fd,
                    struct ev_loop* evLoop,
                    const MessageHandler& onMessage)
-    : mInputWatcher(evLoop),
-      mFD(fd),
-      mOnMessage(onMessage),
-      mState(State::START)
+	: mInputWatcher(evLoop),
+	  mFD(fd),
+	  mOnMessage(onMessage),
+	  mState(State::START)
 {
-    if (!evLoop) {
-        THROW("ev_loop is null");
-    }
+	if (!evLoop) {
+		THROW("ev_loop is null");
+	}
 
-    if (mFD < 0) {
-        THROW("bad fd");
-    }
+	if (mFD < 0) {
+		THROW("bad fd");
+	}
 
-    mInputWatcher.set<Receiver, &Receiver::onInput>(this);
+	mInputWatcher.set<Receiver, &Receiver::onInput>(this);
 }
 
 Receiver::~Receiver()
 {
-    shutdown();
+	shutdown();
 }
 
 void Receiver::start()
 {
-    LOGD("Starting");
-    mInputWatcher.start(mFD, ev::READ);
+	LOGD("Starting");
+	mInputWatcher.start(mFD, ev::READ);
 }
 
 void Receiver::stop()
 {
-    LOGD("Stopping");
-    mInputWatcher.stop();
+	LOGD("Stopping");
+	mInputWatcher.stop();
 }
 
 void Receiver::shutdown()
 {
-    LOGD("Shutting down");
-    if (mFD < 0) {
-        return;
-    }
+	LOGD("Shutting down");
+	if (mFD < 0) {
+		return;
+	}
 
-    // No communication with the socket is possible after shutdown
-    stop();
-    if (-1 == ::shutdown(mFD, SHUT_RD)) {
-        LOGW("shutdown() failed with: " <<  std::strerror(errno));
-    }
-    mFD = -1;
+	// No communication with the socket is possible after shutdown
+	stop();
+	if (-1 == ::shutdown(mFD, SHUT_RD)) {
+		LOGW("shutdown() failed with: " <<  std::strerror(errno));
+	}
+	mFD = -1;
 
-    mMessage.reset();
+	mMessage.reset();
 }
 
 bool Receiver::isClosed()
 {
-    return mFD == -1;
+	return mFD == -1;
 }
 
 void Receiver::onInput(ev::io& w, int revents)
 {
-    LOGD("Received input");
-    if (EV_ERROR & revents) {
-        LOGW("Unspecified error in input callback: " <<  std::strerror(errno));
-        shutdown();
-        return;
-    }
+	try {
 
-    // Make this configurable
-    // TODO: Avoid allocating every time
-    std::vector<char> buf(512);
+		LOGD("Received input");
+		if (EV_ERROR & revents) {
+			LOGW("Unspecified error in input callback: " <<  std::strerror(errno));
+			shutdown();
+			return;
+		}
 
-    ssize_t received = ::read(w.fd, buf.data(), buf.size());
-    if (received < 0) {
-        if (errno == ECONNRESET) {
-            // Connection reset by peer.
-            shutdown();
-            return;
-        }
+		// Make this configurable
+		// TODO: Avoid allocating every time
+		std::vector<char> buf(512);
 
-        if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR) {
-            LOGD("Neglected error when reading the Receiver's socket:" <<  std::strerror(errno));
-            return;
-        }
+		ssize_t received = ::read(w.fd, buf.data(), buf.size());
+		if (received < 0) {
+			if (errno == ECONNRESET) {
+				// Connection reset by peer.
+				shutdown();
+				return;
+			}
 
-        LOGE("Error when reading the Receiver's socket:" <<  std::strerror(errno));
-        shutdown();
-        return;
-    }
+			if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR) {
+				LOGD("Neglected error when reading the Receiver's socket:" <<  std::strerror(errno));
+				return;
+			}
 
-    if (received == 0) {
-        LOGD("Connection closed by peer");
-        shutdown();
-        return;
-    }
+			LOGE("Error when reading the Receiver's socket:" <<  std::strerror(errno));
+			shutdown();
+			return;
+		}
 
-    // This won't reallocate any data, just a trick to work with a vector
-    buf.resize(received);
+		if (received == 0) {
+			LOGD("Connection closed by peer");
+			shutdown();
+			return;
+		}
 
-    parse(buf);
+		// This won't reallocate any data, just a trick to work with a vector
+		buf.resize(received);
+
+		parse(buf);
+	}
+	catch (const std::exception& e) {
+		LOGE("Unexpected exception: " << e.what());
+	}
+	catch (...) {
+		LOGE("Unexpected exception");
+	}
 }
 
 void Receiver::parse(const std::vector<char>& data)
 {
-    LOGD("Parsing message, size: " << data.size());
-    for (size_t i = 0; i < data.size();) {
-        // LOGD("PARSER: State" << (int)mState);
-        switch (mState) {
-        case State::START: {
-            LOGD("PARSER: Message start");
-            mMessage = std::make_shared<Message>();
+	LOGD("Parsing message, size: " << data.size());
+	for (size_t i = 0; i < data.size();) {
+		// LOGD("PARSER: State" << (int)mState);
+		switch (mState) {
+		case State::START: {
+			LOGD("PARSER: Message start");
+			mMessage = std::make_shared<Message>();
 
-            // Prepare for kind
-            mBytesRead = 0;
-            mState = State::KIND;
-            break;
-        }
-        case State::KIND: {
-            reinterpret_cast<char*>(&mMessage->kind)[mBytesRead] = data[i];
+			// Prepare for kind
+			mBytesRead = 0;
+			mState = State::KIND;
+			break;
+		}
+		case State::KIND: {
+			reinterpret_cast<char*>(&mMessage->kind)[mBytesRead] = data[i];
 
-            ++mBytesRead;
-            if (mBytesRead >= sizeof(Message::kind)) {
-                LOGD("PARSER: Kind: " << mMessage->kind);
+			++mBytesRead;
+			if (mBytesRead >= sizeof(Message::kind)) {
+				LOGD("PARSER: Kind: " << mMessage->kind);
 
-                // Prepare for size
-                mBytesRead = 0;
-                mState = State::SIZE;
-            }
+				// Prepare for size
+				mBytesRead = 0;
+				mState = State::SIZE;
+			}
 
-            ++i;
-            break;
-        }
-        case State::SIZE: {
-            // LOGD("PARSER: Size byte: " <<  std::to_string((int)data[i]));
-            // LOGD("PARSER: Size read: " << std::to_string(mBytesRead));
-            reinterpret_cast<char*>(&mMessage->size)[mBytesRead] = data[i];
+			++i;
+			break;
+		}
+		case State::SIZE: {
+			// LOGD("PARSER: Size byte: " <<  std::to_string((int)data[i]));
+			// LOGD("PARSER: Size read: " << std::to_string(mBytesRead));
+			reinterpret_cast<char*>(&mMessage->size)[mBytesRead] = data[i];
 
-            ++mBytesRead;
-            ++i;
-            if (mBytesRead >= sizeof(Message::size)) {
-                // Prepare for body
-                mBytesRead = 0;
-                LOGD("PARSER: Size: " << mMessage->size);
-                if (mMessage->size != 0) {
-                    mState = State::BODY;
-                }
-                else {
-                    mState = State::END;
-                    goto state_end;
-                }
-            }
+			++mBytesRead;
+			++i;
+			if (mBytesRead >= sizeof(Message::size)) {
+				// Prepare for body
+				mBytesRead = 0;
+				LOGD("PARSER: Size: " << mMessage->size);
+				if (mMessage->size != 0) {
+					mState = State::BODY;
+				}
+				else {
+					mState = State::END;
+					goto state_end;
+				}
+			}
 
-            break;
-        }
-        case State::BODY: {
-            // LOGD("PARSER: Body expected: " << mMessage->size << " read: " << mBytesRead);
+			break;
+		}
+		case State::BODY: {
+			// LOGD("PARSER: Body expected: " << mMessage->size << " read: " << mBytesRead);
 
-            std::size_t bodyLeft = mMessage->size - mBytesRead;
-            // bodyLeft = bodyLeft > data.size() - i;
-            std::size_t dataLeft = data.size() - i;
+			std::size_t bodyLeft = mMessage->size - mBytesRead;
+			// bodyLeft = bodyLeft > data.size() - i;
+			std::size_t dataLeft = data.size() - i;
 
 
-            std::size_t toRead = bodyLeft > dataLeft ? dataLeft : bodyLeft;
-            std::copy_n(data.begin() + i, toRead, std::ostream_iterator<char>(mMessage->ss));
-            mBytesRead += toRead;
+			std::size_t toRead = bodyLeft > dataLeft ? dataLeft : bodyLeft;
+			std::copy_n(data.begin() + i, toRead, std::ostream_iterator<char>(mMessage->ss));
+			mBytesRead += toRead;
 
-            if (mMessage->size != mBytesRead) {
-                // Body not yet ready
-                LOGD("BODY not ready");
-                return;
-            }
+			if (mMessage->size != mBytesRead) {
+				// Body not yet ready
+				LOGD("BODY not ready");
+				return;
+			}
 
-            mState = State::END;
-            i += toRead;
+			mState = State::END;
+			i += toRead;
 
-            // NO BREAK HERE
-            // Move to the next state right away
-            // break;
-        }
+			// NO BREAK HERE
+			// Move to the next state right away
+			// break;
+		}
 state_end:
-        case State::END: {
-            LOGD("PARSER: Message end");
-            mOnMessage(mMessage);
-            mState = State::START;
-            break;
-        }
-        }
-    }
+		case State::END: {
+			LOGD("PARSER: Message end");
+			mOnMessage(mMessage);
+			mState = State::START;
+			break;
+		}
+		}
+	}
 
-    LOGD("Parsed all data");
+	LOGD("Parsed all data");
 
 }
 
--- src/internals/sender.cpp
+++ src/internals/sender.cpp
@@ -13,143 +13,153 @@ namespace internals {
 
 
 Sender::Sender(int fd, struct ev_loop* evLoop)
-    : mOutputWatcher(evLoop),
-      mFD(fd),
-      mOutputBufferPosition(0)
+	: mOutputWatcher(evLoop),
+	  mFD(fd),
+	  mOutputBufferPosition(0)
 {
-    if (!evLoop) {
-        THROW("ev_loop is null");
-    }
+	if (!evLoop) {
+		THROW("ev_loop is null");
+	}
 
-    if (mFD < 0) {
-        THROW("bad fd");
-    }
+	if (mFD < 0) {
+		THROW("bad fd");
+	}
 
-    mOutputWatcher.set<Sender, &Sender::onOutput>(this);
+	mOutputWatcher.set<Sender, &Sender::onOutput>(this);
 }
 
 Sender::~Sender()
 {
-    shutdown();
+	shutdown();
 }
 
 void Sender::start()
 {
-    LOGD("Starting");
+	LOGD("Starting");
 
-    if (!mMessages.empty()) {
-        mOutputWatcher.start(mFD, ev::WRITE);
-    }
+	if (!mMessages.empty()) {
+		mOutputWatcher.start(mFD, ev::WRITE);
+	}
 }
 
 void Sender::stop()
 {
-    LOGD("Stopping");
+	LOGD("Stopping");
 
-    mOutputWatcher.stop();
+	mOutputWatcher.stop();
 }
 
 void Sender::shutdown()
 {
-    LOGD("Shutting down");
+	LOGD("Shutting down");
 
-    if (mFD < 0) {
-        return;
-    }
+	if (mFD < 0) {
+		return;
+	}
 
-    // No communication with the socket is possible after shutdown
-    stop();
+	// No communication with the socket is possible after shutdown
+	stop();
 
-    if (-1 == ::shutdown(mFD, SHUT_WR)) {
-        LOGW("shutdown() failed with: " <<  std::strerror(errno));
-    }
-    mFD = -1;
+	if (-1 == ::shutdown(mFD, SHUT_WR)) {
+		LOGW("shutdown() failed with: " <<  std::strerror(errno));
+	}
+	mFD = -1;
 
-    while (!mMessages.empty()) {
-        mMessages.pop();
-    }
+	while (!mMessages.empty()) {
+		mMessages.pop();
+	}
 
-    mOutputBuffer.clear();
-    mOutputBuffer.shrink_to_fit();
+	mOutputBuffer.clear();
+	mOutputBuffer.shrink_to_fit();
 }
 
 bool Sender::isClosed()
 {
-    return mFD == -1;
+	return mFD == -1;
 }
 
 void Sender::fillBuffer()
 {
-    auto& message = *mMessages.front();
+	auto& message = *mMessages.front();
 
-    LOGD("Serialize message: kind:" << message.kind);
-    mOutputBuffer = message.getData();
+	LOGD("Serialize message: kind:" << message.kind);
+	mOutputBuffer = message.getData();
 
-    mMessages.pop();
+	mMessages.pop();
 }
 
 void Sender::onOutput(ev::io& w, int revents)
 {
-    if (EV_ERROR & revents) {
-        LOGE("Unspecified error in output callback: " <<  std::strerror(errno));
-        shutdown();
-        return;
-    }
+	try {
 
 
-    // Ensure output buffer has any data to send
-    if (mOutputBufferPosition >= mOutputBuffer.size()) {
-        // No data to send in mOutputBuffer.
-        LOGD("POS: " << mOutputBufferPosition);
+		if (EV_ERROR & revents) {
+			LOGE("Unspecified error in output callback: " <<  std::strerror(errno));
+			shutdown();
+			return;
+		}
 
-        if (mMessages.empty()) {
-            LOGD("EMPTY");
-            // And there's no more messages to send.
-            // Pause sending and free the buffer.
-            mOutputBufferPosition = 0;
-            mOutputBuffer.clear();
-            mOutputWatcher.stop();
-            return;
-        }
 
-        // Fill mOutputBuffer with the next serialized Response from the queue.
-        mOutputBufferPosition = 0;
-        mOutputBuffer.resize(0);
-        fillBuffer();
-    }
+		// Ensure output buffer has any data to send
+		if (mOutputBufferPosition >= mOutputBuffer.size()) {
+			// No data to send in mOutputBuffer.
+			LOGD("POS: " << mOutputBufferPosition);
 
-    ssize_t n  = ::write(w.fd,
-                         &mOutputBuffer[mOutputBufferPosition],
-                         mOutputBuffer.size() - mOutputBufferPosition);
-    if (n >= 0) {
-        mOutputBufferPosition += n;
-    }
-    else if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR) {
-        // Neglected errors
-    }
-    else {
-        if (errno == ECONNRESET) {
-            // Connection reset by peer.
-            shutdown();
-            return;
-        }
-        LOGE("write() failed with: " <<  std::strerror(errno));
-        shutdown();
-        return;
-    }
+			if (mMessages.empty()) {
+				LOGD("EMPTY");
+				// And there's no more messages to send.
+				// Pause sending and free the buffer.
+				mOutputBufferPosition = 0;
+				mOutputBuffer.clear();
+				mOutputWatcher.stop();
+				return;
+			}
 
-    if (n == 0) {
-        shutdown();
-    }
+			// Fill mOutputBuffer with the next serialized Response from the queue.
+			mOutputBufferPosition = 0;
+			mOutputBuffer.resize(0);
+			fillBuffer();
+		}
+
+		ssize_t n  = ::write(w.fd,
+		                     &mOutputBuffer[mOutputBufferPosition],
+		                     mOutputBuffer.size() - mOutputBufferPosition);
+		if (n >= 0) {
+			mOutputBufferPosition += n;
+		}
+		else if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR) {
+			// Neglected errors
+		}
+		else {
+			if (errno == ECONNRESET) {
+				// Connection reset by peer.
+				shutdown();
+				return;
+			}
+			LOGE("write() failed with: " <<  std::strerror(errno));
+			shutdown();
+			return;
+		}
+
+		if (n == 0) {
+			shutdown();
+		}
+	}
+	catch (const std::exception& e) {
+		LOGE("Unexpected exception: " << e.what());
+	}
+	catch (...) {
+		LOGE("Unexpected exception");
+	}
 }
 
 void Sender::send(const std::shared_ptr<yodle::Message> message)
 {
-    LOGD("Sending message: kind: " << message->kind);
-    mMessages.push(message);
+	LOGD("Sending message: kind: " << message->kind);
+	mMessages.push(message);
 
-    // Ensure sending data is switched on
-    mOutputWatcher.start(mFD, ev::WRITE);
+	// Ensure sending data is switched on
+	mOutputWatcher.start(mFD, ev::WRITE);
 }
 
 } // namespace internals
